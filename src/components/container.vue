4ec413d95cb9e6311b0f52739bc20aa7a3b5a798
ithiel883 <ajuka.zephiniah@gmail.com>
2023-04-28 05:19:48 +0100

    task implementation

--- src/components/container.vue
+++ src/components/container.vue
@@ -1,20 +1,129 @@
 <template>
-    <div class="container">
-        <span class="label">Container Box</span>
-        <content/>
+  <div
+    class="container"
+    :style="{ left: `${containerX}px`, top: `${containerY}px` }"
+    @mousedown="startContainerDrag"
+    @mousemove="containerDrag"
+    @mouseup="endContainerDrag"
+  >
+  <span class="label">Container Box</span>
+
+    <div
+      class="draggable"
+      :style="{ left: `${x}px`, top: `${y}px` }"
+      @mousedown="startDrag"
+      @mousemove="drag"
+      @mouseup="endDrag"
+    >
+    <div class="content">
+      <span class="content-label">Content Box</span>
+   </div>
     </div>
+  </div>
 </template>
 
-<script lang='ts' setup>
-    import content from './content.vue';
+<script lang="ts">
+import { ref } from 'vue';
+
+export default {
+  name: 'DraggableElement',
+  setup() {
+    const dragging = ref(false);
+    const startX = ref(0);
+    const startY = ref(0);
+    const x = ref(0);
+    const y = ref(0);
+
+    const startDrag = (event: MouseEvent) => {
+      event.preventDefault();
+      dragging.value = true;
+      startX.value = event.clientX - x.value;
+      startY.value = event.clientY - y.value;
+    };
+
+    const drag = (event: MouseEvent) => {
+      event.preventDefault();
+      if (dragging.value) {
+        x.value = event.clientX - startX.value;
+        y.value = event.clientY - startY.value;
+      }
+    };
+
+    const endDrag = () => {
+      dragging.value = false;
+    };
+
+    const containerDragging = ref(false);
+    const containerStartX = ref(0);
+    const containerStartY = ref(0);
+    const containerX = ref(0);
+    const containerY = ref(0);
+
+    const startContainerDrag = (event: MouseEvent) => {
+      event.preventDefault();
+      containerDragging.value = true;
+      containerStartX.value = event.clientX - containerX.value;
+      containerStartY.value = event.clientY - containerY.value;
+    };
+
+    const containerDrag = (event: MouseEvent) => {
+      event.preventDefault();
+      if (containerDragging.value) {
+        containerX.value = event.clientX - containerStartX.value;
+        containerY.value = event.clientY - containerStartY.value;
+      }
+    };
+
+    const endContainerDrag = () => {
+      containerDragging.value = false;
+    };
+
+    return {
+      dragging,
+      startX,
+      startY,
+      x,
+      y,
+      startDrag,
+      drag,
+      endDrag,
+      containerDragging,
+      containerStartX,
+      containerStartY,
+      containerX,
+      containerY,
+      startContainerDrag,
+      containerDrag,
+      endContainerDrag,
+    };
+  },
+};
 </script>
 
-<style lang='postcss' scoped>
-    .container {
-        @apply w-96 h-80 bg-gray-300 flex items-center justify-center relative;
+<style  scoped>
+
+.content  {
+      @apply bg-black h-24 w-36 relative;
+   }
+ .content-label {
+       @apply absolute -top-3 px-3 left-0 bg-black text-white text-xs
     }
 
+.draggable {
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  color: #fff;
+  text-align: center;
+  line-height: 100px;
+  cursor: move;
+}
+
+.container {
+        @apply w-96 h-80 bg-gray-300 flex items-center justify-center relative;
+    }
     .label {
        @apply absolute -top-3 px-3 left-0 bg-gray-300 text-black text-xs;
     }
+
 </style>
--- src/components/content.vue
+++ src/components/content.vue
@@ -1,18 +1,10 @@
 <template>
-   <div class="content">
-      <span class="label">Content Box</span>
-   </div>
+<div>
+  this is a level
+</div>
+
 </template>
 
-<script lang='ts' setup>
-    
-</script>
+<script lang="ts">
 
-<style lang='postcss' scoped>
-   .content  {
-      @apply bg-black h-24 w-36 relative;
-   }
- .label {
-       @apply absolute -top-3 px-3 left-0 bg-black text-white text-xs
-    }
-</style>
+</script>
